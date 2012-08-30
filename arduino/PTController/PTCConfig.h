0c80feb488bd55f77673bea52cbf88adf0f13676
John Stowers <john.stowers@gmail.com>
2012-08-30 13:06:14 +0200

    PTC: use arduino-mk, separate config

---
+++ arduino/PTController/PTCConfig.h
@@ -0,0 +1,36 @@
+#ifndef _PTC_CONFIG_H
+#define _PTC_CONFIG_H
+
+#include <Ethernet.h>
+
+#define PTC_DEBUG               1
+#define PTC_DEBUG_DYNAMIXEL     0
+
+#define PTC_DEBUG_SERIAL_BAUD   57600
+
+#define PTC_LED_STATUS          8
+#define PTC_LED_COMMAND         7
+#define PTC_COMM_DIR_PIN        2
+
+#define PTC_RXBUF_START         '@'
+#define PTC_RXBUF_START_IDX     0
+#define PTC_RXBUF_CMD_IDX       1
+#define PTC_RXBUF_TYPE_IDX      2
+#define PTC_RXBUF_DATA_L_IDX    3
+#define PTC_RXBUF_DATA_H_IDX    4
+#define PTC_RXBUF_END_IDX       5
+#define PTC_RXBUF_END           '='
+#define PTC_RXBUF_LEN           6
+uint8_t ptc_rxbuf[PTC_RXBUF_LEN];
+
+byte ptc_mac[] = {0x90, 0xA2, 0xDA, 0x00, 0x79, 0xDF};
+IPAddress ptc_ip(192,168,1,60);
+#define     PTC_TCP_PORT        23
+
+#define PTC_SERVO_ID_PAN        1
+#define PTC_SERVO_ID_TILT       2
+#define PTC_SERVO_BAUD          57600
+
+#define PTC_PIN_LASER           9
+
+#endif /* _PTC_CONFIG_H */
--- arduino/PTController/PTController.ino
+++ arduino/PTController/PTController.ino
@@ -1,83 +1,138 @@
-
+// vi: set shiftwidth=2 tabstop=2 expandtab:
 
 #include <DynamixelSerial.h>
-#include <SPI.h>
 #include <Ethernet.h>
 
+#include "PTCConfig.h"
+
 class DynamixelSerialPTC : public DynamixelSerial {
-    void sendData(unsigned char d) {
-      Serial1.write(d);
-//      Serial.write(d);  // write to other serial port for debugging
-      Serial1.flush();
-    }
-    int availableData(void) {return Serial1.available();}
-    int readData(void) {return Serial1.read();}
-    int peekData(void) {return Serial1.peek();}
-    void beginCom(void) {Serial1.begin(57600);}
-    void endCom(void) {Serial1.end();}
+  void sendData(unsigned char d) {
+    Serial1.write(d);
+#if PTC_DEBUG_DYNAMIXEL
+    Serial.write(d);
+#endif
+    Serial1.flush();
+  }
+  int availableData(void) {return Serial1.available();}
+  int readData(void)      {return Serial1.read();}
+  int peekData(void)      {return Serial1.peek();}
+  void beginCom(void)     {Serial1.begin(PTC_SERVO_BAUD);}
+  void endCom(void)       {Serial1.end();}
 };
 
-
-byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x79, 0xDF };
-IPAddress ip(192,168,1,60);
-
-const int LED_SERVO   = 8;
-const int LED_COMMAND = 7;
-const int PTC_COMM_DIR_PIN = 2;
-
-DynamixelSerialPTC pantilt;
-EthernetServer server(23);
+DynamixelSerialPTC  pantilt;
+EthernetServer      server(PTC_TCP_PORT);
 
 void setup() {
-  pinMode(LED_SERVO, OUTPUT);
-  pinMode(LED_COMMAND, OUTPUT);
+  pinMode(PTC_LED_STATUS, OUTPUT);
+  pinMode(PTC_LED_COMMAND, OUTPUT);
+  pinMode(PTC_PIN_LASER, OUTPUT);
 
-  Ethernet.begin(mac, ip);
+  Ethernet.begin(ptc_mac, ptc_ip);
   server.begin();
 
-  Serial.begin(57600);
+#if PTC_DEBUG_DYNAMIXEL || PTC_DEBUG
+  Serial.begin(PTC_DEBUG_SERIAL_BAUD);
+#endif
   
   pantilt.begin(PTC_COMM_DIR_PIN);
 }
 
+uint8_t ethernet_parse(EthernetClient &client)
+{
+    int i = 0;
+    uint8_t ok = 0;
+
+    //read all the data from the tcp packet
+#if PTC_DEBUG
+    Serial.print(" RX ");
+#endif
+    while (client.available() && client.connected()) {
+      int c = client.read();
+      if (c == -1)
+        break;
+
+      ptc_rxbuf[i++] = c;
+#if PTC_DEBUG
+      Serial.print(c);
+      Serial.print(' ');
+#endif
+      if (i == PTC_RXBUF_LEN) {
+        ok = 1;
+        break;
+      }
+    }
+#if PTC_DEBUG
+    Serial.print("\r\n");
+#endif
+
+    return ok;
+}
+
+uint8_t command_parse(uint8_t &cmd, uint8_t &type, uint16_t &val)
+{
+  if ((ptc_rxbuf[PTC_RXBUF_START_IDX] == PTC_RXBUF_START) && \
+      (ptc_rxbuf[PTC_RXBUF_END_IDX] == PTC_RXBUF_END)) {
+    cmd = ptc_rxbuf[PTC_RXBUF_CMD_IDX];
+    type = ptc_rxbuf[PTC_RXBUF_TYPE_IDX];
+    val = *((uint16_t *)(&ptc_rxbuf[PTC_RXBUF_DATA_L_IDX]));
+    return 1;
+  }
+  return 0;
+}
+
 void loop() {
-  static boolean clientConnected = false;
-  static int pos = 0;
-  int ok;
+  uint8_t rxok = 0;
 
   EthernetClient client = server.available();
   if (client) {
-    if (!clientConnected) {
-      Serial.println("Client Connected");
-      clientConnected = true;
-    }
-    
-    if (client.available()) {
-      char c = client.read();
+    rxok = ethernet_parse(client);
+  }
 
+  if (rxok) {
+    uint8_t cmd, type;
+    uint16_t val;
+    int cmdok;
+
+    cmdok = command_parse(cmd, type, val);
+    if (cmdok) {
       //toggle activity LED
-      digitalWrite(LED_COMMAND, digitalRead(LED_COMMAND) ? LOW : HIGH);
-    
-      if (c == 'H') {
-        digitalWrite(LED_SERVO, HIGH);
-      } else if (c == 'L') {
-        digitalWrite(LED_SERVO, LOW);
-      } else if (c == 'M') {
-        ok = pantilt.move(1,pos);
-        pos += 10;
-      } else if (c == 'Z') {
-        unsigned char buf[] = {0xFF,0xFF,0x01,0x05,0x03,0x1E,0xFF,0x0F,0xCA};
-        digitalWrite(PTC_COMM_DIR_PIN,1);
-        Serial1.write(buf, sizeof(buf));
-        Serial1.flush();
-        delayMicroseconds(400);
-        digitalWrite(PTC_COMM_DIR_PIN,0);
+      digitalWrite(PTC_LED_COMMAND, digitalRead(PTC_LED_COMMAND) ? LOW : HIGH);
+      switch (cmd) {
+        case 'S':
+          digitalWrite(PTC_LED_STATUS, HIGH);
+          break;
+        case 's':
+          digitalWrite(PTC_LED_STATUS, LOW);
+          break;
+        case 'L':
+          digitalWrite(PTC_PIN_LASER, HIGH);
+          break;
+        case 'l':
+          digitalWrite(PTC_PIN_LASER, LOW);
+          break;
+        case 'P':
+          cmdok = pantilt.move(PTC_SERVO_ID_PAN, val);
+          break;
+        case 'T':
+          cmdok = pantilt.move(PTC_SERVO_ID_TILT, val);
+          break;
+        default:
+          cmdok = 0;
+          break;
       }
+       
+  #if PTC_DEBUG
       Serial.print("CMD: ");
-      Serial.print(c);
+      Serial.print(cmd);
+      Serial.print(" TYPE: 0x");
+      Serial.print(type, HEX);
+      Serial.print(" VAL: 0x");
+      Serial.print(val, HEX);
       Serial.print(" RES: ");
-      Serial.print(ok);
+      Serial.print(cmdok);
       Serial.print("\r\n");
+  #endif
     }
   }
 }
